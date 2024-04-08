acd00d980a9b2d73696e836a2b7f90441a2d00f0
MicheleData <162191791+MicheleData@users.noreply.github.com>
2024-04-08 11:09:06 -0300

    Add files via upload

--- api-rh.py
+++ api-rh.py
@@ -1,27 +1,14 @@
 from flask import Flask
 import pandas as pd
-import pyodbc
 
-dados_conexao = ("Driver={SQL Server};"
-                    "Server=DESKTOP-DFB74LT;"
-                    "DataBase=Base_RH")
-conexao = pyodbc.connect(dados_conexao)
-cursor = conexao.cursor()
+df_rh = pd.read_excel("BaseFuncionarios.xlsx")
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def rh():
-    comando = f""" SELECT * FROM Tabela_base
-"""
     
-    cursor.execute(comando)
-    dados = cursor.fetchall()
-    descricao = cursor.description
-    colunas = [tupla[0] for tupla in descricao]
-
-    df_rh = pd.DataFrame.from_records(dados, columns=colunas)
     df_rh_json = df_rh.to_json()
 
     return df_rh_json
